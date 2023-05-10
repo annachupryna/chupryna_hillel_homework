973cef717a3fd9152b3b71c4200bcf6c955bea0c
annachupryna <anna.chupryna19@gmail.com>
2023-05-10 21:18:09 +0200

    finished task 4

---
+++ task_4.py
@@ -0,0 +1,5 @@
+#4
+vegetarians = ["Serhiy", "Anastasiia", "Olena", "Oksana", "Maksym", "Nadiia"]
+omnivores = ["Stepan", "Vasyl", "Solomiia", "Polina", "Anhelina"]
+omnivores.extend(vegetarians)
+print(vegetarians)
