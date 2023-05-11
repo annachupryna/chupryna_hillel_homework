620208fcc41108136cf22a0221f8bd3ab36bf881
annachupryna <anna.chupryna19@gmail.com>
2023-05-11 17:23:37 +0200

    fixed comments for task 6

--- task_6.py
+++ task_6.py
@@ -1,9 +1,4 @@
 #6
 names = ["Tyler Craig", "Nicola Suarez", "Tyler Craig", "Claire Chang", "Suzanne Mejia", "Claire Chang", "Tyler Craig"]
-unique_names = []
-
-for name in names:
-    if name not in unique_names:
-        unique_names.append(name)
-
-print(unique_names)
+unique_list = dict().fromkeys(names)
+print(unique_list)
