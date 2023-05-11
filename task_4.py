8fd34da8e2cd573e7875764790b6fdd44233b317
annachupryna <anna.chupryna19@gmail.com>
2023-05-11 17:21:28 +0200

    fixed comments for task 4

--- task_4.py
+++ task_4.py
@@ -1,5 +1,13 @@
 #4
-vegetarians = ["Serhiy", "Anastasiia", "Olena", "Oksana", "Maksym", "Nadiia"]
-omnivores = ["Stepan", "Vasyl", "Solomiia", "Polina", "Anhelina"]
-omnivores.extend(vegetarians)
-print(vegetarians)
+anna = {'name': 'Anna', 'vegetarian': True, 'omnivore': True}
+oleh = {'name': 'Oleh', 'vegetarian': True, 'omnivore': True}
+olena = {'name': 'Olena', 'vegetarian': True, 'omnivore': False}
+oksana = {'name': 'Oksana', 'vegetarian': True, 'omnivore': False}
+people = [olena, oksana, anna, oleh]
+guests_herbs_vegetables = set()
+for person in people:
+    for key, value in person.items():
+        if person.get('vegetarian'):
+            guests_herbs_vegetables.add(person.get('name'))
+
+print(guests_herbs_vegetables)
