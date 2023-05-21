7aae0a441819e88bb3f3e8b89c0783fd66edc5fa
annachupryna <anna.chupryna19@gmail.com>
2023-05-21 09:35:53 +0200

    fixed comments for task 1

--- Homework_5/task_1.py
+++ Homework_5/task_1.py
@@ -4,11 +4,10 @@ import pickle
 
 # 1
 list_of_tuples = []
-for number in range(1, 101):
+for _ in range(1, 101):
     list_of_tuples.append((random.randint(1, 100), random.randint(1, 100), random.randint(1, 3)))
 
-os.makedirs('./Homework_5.1')
-os.chdir('./Homework_5.1')
-with open('data.txt', 'w+b') as file:
+os.makedirs('./test/data')
+with open('./test/data/data.txt', 'w+b') as file:
     data = pickle.dumps(list_of_tuples)
     file.write(data)
