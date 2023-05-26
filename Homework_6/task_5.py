c353fcf5b0257ea84e0dff474496a1185acd6fe6
annachupryna <anna.chupryna19@gmail.com>
2023-05-26 20:01:47 +0200

    finished task 5

---
+++ Homework_6/task_5.py
@@ -0,0 +1,8 @@
+"""
+    Task 5
+    Create a function that accepts N parameters of type int. Calculate the sum of these parameters and return it.
+"""
+
+
+def calculate_params(*args: int):
+    return len(args)
